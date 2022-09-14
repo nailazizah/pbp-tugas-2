3acbbee2448dbaf8c6561e55f1e6febce758d9e2
Naila Azizah <naila.azizah@ui.ac.id>
2022-09-14 15:44:42 +0700

    Tugas 2: Deploy katalog

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
 # TODO: Create your views here.
+from katalog.models import CatalogItem
+
+# Create your views here.
+def show_katalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_catalog_item,
+        'nama': 'Naila Azizah',
+        'studentid' : '2106705814'
+
+    }
+    return render(request, "katalog.html", context)
+
